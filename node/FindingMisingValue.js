60cbf4bd30062f32121c642f507ff7e1529ae782
farookfazni <faznifarook@gmail.com>
2022-06-02 16:37:16 +0530

    Finding missing value using Node 2

--- node/FindingMisingValue.js
+++ node/FindingMisingValue.js
@@ -1,15 +1,15 @@
-var arr = [ 26,24,25,22,21,27,30,29,23 ];
+var arr = [ 26,24,25,22,21,27,28,29,30 ];
 arr.sort()
 // console.log(arr);
 var i,x,y;
 
-for (let i = 0; i < arr.length-1; i++) {
+for (let i = 0; i < arr.length; i++) {
     x = arr[i];
     y = arr[i+1];
     if(x+1 != y){
         console.log(x+1);
         break;
-    }else if((x+1 == y) && (i==arr.length-2)){
+    }else if((x+1 == y) && (i==arr.length-1)){
         console.log(y+1);
     }
   }
