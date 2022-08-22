c3df86ee90a19e89549f1186205efc0a6eee906f
farookfazni <faznifarook@gmail.com>
2022-08-22 11:41:49 +0530

    project intialized

---
+++ GasStationApp/src/test/java/com/faznifarook/GasStationApp/GasStationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faznifarook.GasStationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasStationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
